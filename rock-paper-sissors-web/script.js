91ee889096edd6e412c351e9a41459433384d0b3
Ayush Pandey <32012449+itsayushpandey@users.noreply.github.com>
2024-01-11 13:56:24 -0700

    Add files via upload

---
+++ rock-paper-sissors-web/script.js
@@ -0,0 +1,33 @@
+﻿function playGame(playerChoice) {
+    const choices = ['rock', 'paper', 'scissors'];
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+
+
+    let result;
+
+
+    if (playerChoice === computerChoice) {
+        result = 'It\'s a tie!';
+    } else if (
+        (playerChoice === 'rock' && computerChoice === 'scissors') ||
+        (playerChoice === 'paper' && computerChoice === 'rock') ||
+        (playerChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        result = 'You win!';
+    } else {
+        result = 'You lose!';
+    }
+
+
+    displayResult(result, playerChoice, computerChoice);
+}
+
+
+function displayResult(result, playerChoice, computerChoice) {
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = `
+        <p>Your Choice: ${playerChoice}</p>
+        <p>Computer's Choice: ${computerChoice}</p>
+        <p>${result}</p>
+    `;
+}
